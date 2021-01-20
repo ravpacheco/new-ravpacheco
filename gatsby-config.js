d2472b32b35242c984beadfab999e7c37ab9d4d6
Rafael Pacheco <ravpacheco@gmail.com>
2021-01-19 21:29:18 -0300

    Add some metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `Pacheco - Website`,
+    name: `Rafael Pacheco`,
+    siteUrl: `https://ravpacheco.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
       heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
@@ -11,23 +11,19 @@ module.exports = {
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/ravpachecco`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/ravpacheco`,
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/ravpachecco`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://www.linkedin.com/in/ravpacheco/`,
       },
     ],
   },
